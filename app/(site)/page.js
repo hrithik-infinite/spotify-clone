ebbcec1e3195febe2c48447b7206c30cc8fecdc2
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-09-17 10:05:59 +0530

    completed basic layout

--- app/(site)/page.js
+++ app/(site)/page.js
@@ -22,6 +22,14 @@ export default function Home() {
           </div>
         </div>
       </Header>
+      <div
+        className="
+      mt-2 mb-7 px-6">
+        <div className=" flex items-center justify-between">
+          <h1 className="text-white text-2xl font-semibold">Newest Song</h1>
+        </div>
+        <div>List of Songs!</div>
+      </div>
     </div>
   );
 }
