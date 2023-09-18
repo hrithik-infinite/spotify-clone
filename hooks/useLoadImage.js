ddf59faeff4f520548728a276e9a6c0627267bd9
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-09-18 12:21:45 +0530

    added songs cards on main screen

---
+++ hooks/useLoadImage.js
@@ -0,0 +1,13 @@
+const { useSupabaseClient } = require("@supabase/auth-helpers-react");
+
+const useLoadImage = (song) => {
+  const supabaseClient = useSupabaseClient();
+  if (!song) {
+    return null;
+  }
+
+  const { data: imageData } = supabaseClient.storage.from("images").getPublicUrl(song.image_path);
+  return imageData.publicUrl;
+};
+
+export default useLoadImage;
