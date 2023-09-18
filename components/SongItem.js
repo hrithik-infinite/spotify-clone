affc4c5f66f965009f6ceb78db16c6c3884e9ab5
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-09-18 12:52:18 +0530

    added playbutton

--- components/SongItem.js
+++ components/SongItem.js
@@ -2,6 +2,7 @@
 
 import useLoadImage from "@/hooks/useLoadImage";
 import Image from "next/image";
+import PlayButton from "./PlayButton";
 
 const SongItem = ({ data, onClick }) => {
   const imagePath = useLoadImage(data);
@@ -33,10 +34,13 @@ const SongItem = ({ data, onClick }) => {
       </div>
       <div
         className="
-      flex flex-col items-start first-letter:w-full p-4 gap-y-1">
+      flex flex-col items-start first-letter:w-full pt-4 gap-y-1">
         <p className="font-semibold truncate w-full">{data.title}</p>
         <p className="text-neutral-400 text-sm pb-4 w-full truncate">By {data.author}</p>
       </div>
+      <div className="
+      absolute
+      bottom-24 right-5"><PlayButton /></div>
     </div>
   );
 };
