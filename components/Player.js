7fdebe03ee1fea24f83d325becd2204c0a113295
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-09-20 22:05:36 +0530

    added icons to player

--- components/Player.js
+++ components/Player.js
@@ -3,6 +3,7 @@
 import usePlayer from "@/hooks/UsePlayer";
 import useGetSongById from "@/hooks/useGetSongById";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
+import PlayerContent from "./PlayerContent";
 const Player = () => {
   const player = usePlayer();
   const { song } = useGetSongById(player.activeId);
@@ -15,7 +16,7 @@ const Player = () => {
     <div
       className="
   fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
-      Player
+      <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
     </div>
   );
 };
