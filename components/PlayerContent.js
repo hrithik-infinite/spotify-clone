c725cf51c353a6aa036d38af8c07f7dc84d18509
Hrithik Agarwal <hrithik.agarwal@oracle.com>
2023-09-20 22:15:57 +0530

    added next prev function

--- components/PlayerContent.js
+++ components/PlayerContent.js
@@ -6,10 +6,39 @@ import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
 import Slider from "./Slider";
+import usePlayer from "@/hooks/UsePlayer";
+import { useState } from "react";
 
 const PlayerContent = ({ song, songUrl }) => {
-  const Icon = true ? BsPauseFill : BsPlayFill;
-  const VolumeIcon = false ? HiSpeakerXMark : HiSpeakerWave;
+  const player = usePlayer();
+  const [volume, setVolume] = useState(1);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const Icon = isPlaying ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+  const onPlayNext = () => {
+    if (player.ids.length === 0) {
+      return;
+    }
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const nextSong = player.ids[currentIndex + 1];
+    if (!nextSong) {
+      return player.setId(player.ids[0]);
+    }
+    player.setId(nextSong);
+  };
+
+  const onPlayPrevious = () => {
+    if (player.ids.length === 0) {
+      return;
+    }
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const previousSong = player.ids[currentIndex - 1];
+    if (!previousSong) {
+      return player.setId(player.ids[player.ids.length - 1]);
+    }
+    player.setId(previousSong);
+  };
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -54,7 +83,7 @@ const PlayerContent = ({ song, songUrl }) => {
             max-w-[722px] 
             gap-x-6">
         <AiFillStepBackward
-          onClick={() => {}}
+          onClick={onPlayPrevious}
           size={30}
           className="
               text-neutral-400 
@@ -79,7 +108,7 @@ const PlayerContent = ({ song, songUrl }) => {
           <Icon size={30} className="text-black" />
         </div>
         <AiFillStepForward
-          onClick={() => {}}
+          onClick={onPlayNext}
           size={30}
           className="
               text-neutral-400 
